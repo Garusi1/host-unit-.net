3455a9874ea334c42a30acfbcc63c17c4a0a010a
Garusi <mgarusi101@gmail.com>
2019-12-29 15:44:03 +0200

    added configure with serial Id

---
+++ Project01_0605_5734_dotNet5780/DAL/Configure.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+   public class Configure
+    {
+        private static int OrderID = 10000000;
+        private static int hostUnitID = 20000000;
+        private static int hostID = 30000000;
+        private static int geustReqID = 40000000;
+
+        public int getOrderId()
+        {
+            OrderID++;
+            return OrderID;
+        }
+
+        public int getHostUnitID()
+        {
+            hostUnitID++;
+            return hostUnitID;
+        }
+
+        public int getHostID()
+        {
+            hostID++;
+            return hostID;
+        }
+
+        public int getGeustReqID()
+        {
+            geustReqID++;
+            return geustReqID;
+        }
+
+    }
+}
