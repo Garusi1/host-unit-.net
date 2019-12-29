4a97219e1429a2e8cdefd64020548e0f5fe50461
Garusi <mgarusi101@gmail.com>
2019-12-29 12:17:55 +0200

    adding DS

---
+++ Project01_0605_5734_dotNet5780/DS/DataSource.cs
@@ -0,0 +1,16 @@
+﻿using BE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS
+{
+    class DataSource
+    {
+        public static List<GuestRequest> GuestRequestList;
+        public static List<HostingUnit> HostingUnitList;
+
+    }
+}
