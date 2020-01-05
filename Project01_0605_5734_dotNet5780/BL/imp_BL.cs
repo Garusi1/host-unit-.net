a9459d801822e989e63f8dc1efa577747a2e55bf
Garusi <mgarusi101@gmail.com>
2020-01-05 20:17:10 +0200

    update ui + set functions in  bank details

--- Project01_0605_5734_dotNet5780/BL/imp_BL.cs
+++ Project01_0605_5734_dotNet5780/BL/imp_BL.cs
@@ -31,32 +31,23 @@ namespace BL
 
 
             if (guest.Adults < 0)
-                throw new System.ArgumentException(/*מספר מבוגרים אינו יכול להיות שלילי"*/"Number of adults cannot be negative");
+                throw new Exception(/*מספר מבוגרים אינו יכול להיות שלילי"*/"Number of adults cannot be negative");
             if (guest.Adults == 0)
-                throw new System.ArgumentException(/*מספר מבוגרים אינוי יכול להיות 0"*/"Number of adults cannot be 0");
+                throw new Exception(/*מספר מבוגרים אינוי יכול להיות 0"*/"Number of adults cannot be 0");
             if (guest.Type == BE.TypeEnum.Unknown)
-                throw new System.ArgumentException(/*חובה לבחור סוג יחידת יחידת אירוח"*/"Select unit type of hosting unit is required");
+                throw new Exception(/*חובה לבחור סוג יחידת יחידת אירוח"*/"Select unit type of hosting unit is required");
 
             //AttractionsEnum // Unknown,הכרחי, אפשרי, לא_מעוניין
             if (guest.Pool == BE.AttractionsEnum.Unknown)
-                throw new System.ArgumentException(/*חובה לבחור האם מעוניין בבריכה"*/"Must choose whether you want a pool");
+                throw new Exception(/*חובה לבחור האם מעוניין בבריכה"*/"Must choose whether you want a pool");
             if (guest.Jacuzzi == BE.AttractionsEnum.Unknown)
-                throw new System.ArgumentException(/*חובה לבחור האם מעוניין בג'קוזי"*/"Must choose whether you want a jacuzzi");
+                throw new Exception(/*חובה לבחור האם מעוניין בג'קוזי"*/"Must choose whether you want a jacuzzi");
             if (guest.Garden == BE.AttractionsEnum.Unknown)
-                throw new System.ArgumentException(/*חובה לבחור האם מעוניין בגינה"*/"Must choose whether you want a garden");
+                throw new Exception(/*חובה לבחור האם מעוניין בגינה"*/"Must choose whether you want a garden");
             if ((guest.ChildrensAttractions == BE.AttractionsEnum.Unknown) && (guest.Children > 0))
-                throw new System.ArgumentException(/*חובה לבחור האם מעוניין באטראקציות לילדים"*/"Must choose whether you want a Childrens Attractions");
+                throw new Exception(/*חובה לבחור האם מעוניין באטראקציות לילדים"*/"Must choose whether you want a Childrens Attractions");
 
-            if (checkDateLegallOneYear(guest.EntryDate)) //אם התאריכים חורגים מהטווח של חודש אחורה ו11 חודש קדימה
-            {
-                throw new System.ArgumentException(string.Format("worng input {0} not in the span of dates ", guest.EntryDate));
-
-            }
-            if (checkDateLegallOneYear(guest.ReleaseDate))
-            {
-                throw new System.ArgumentException(string.Format("worng input {0} not in the span of dates ", guest.ReleaseDate));
-            }
-            
+            checkDateLegallOneYear(DateTime generalDate);
 
 
             //מילוי ערכים שגויים
@@ -64,13 +55,13 @@ namespace BL
             theDateToday = DateTime.Now;
             theDateToday = new DateTime(theDateToday.Year, theDateToday.Month, theDateToday.Day); //איפוס שעון ל00:00:00
             if (guest.EntryDate < theDateToday)
-                throw new System.ArgumentException(/*תאריך כניסה אינו יכול להיות  מוקדם מעכשיו"*/"EntryDate cannot be earlier from now ");
+                throw new Exception(/*תאריך כניסה אינו יכול להיות  מוקדם מעכשיו"*/"EntryDate cannot be earlier from now ");
 
             if (guest.ReleaseDate < theDateToday.AddDays(1))
-                throw new System.ArgumentException(/*תאריך יציאה אינו יכול להיות  מוקדם מעוד יום"*/"ReleaseDate cannot be earlier from tomorrow ");
+                throw new Exception(/*תאריך יציאה אינו יכול להיות  מוקדם מעוד יום"*/"ReleaseDate cannot be earlier from tomorrow ");
 
             if (guest.Status != BE.StatusGREnum.פתוחה)
-                throw new System.ArgumentException(/* "סטטוס דרישת לקוח שגוי.סטטוס דרישה חדשה יהיה תמיד פתוח"*/"Incorrect GuestRequest status. New GuestRequest status will always be open ");
+                throw new Exception(/* "סטטוס דרישת לקוח שגוי.סטטוס דרישה חדשה יהיה תמיד פתוח"*/"Incorrect GuestRequest status. New GuestRequest status will always be open ");
 
 
             // after we cheek all the possible problems we can transfer the data to DAL layer
@@ -89,9 +80,9 @@ namespace BL
             if (hostUnit.Owner == null || hostUnit.HostingUnitName == "")
                 throw new Exception("חובה למלא את כל השדות");
             if (!Enum.IsDefined(typeof(AreaEnum), hostUnit.Area))
-                throw new System.ArgumentException("Enum input illegal");
+                throw new Exception("Enum input illegal");
             if (hostUnit.Area == AreaEnum.All)
-                throw new System.ArgumentException("Enum input illegal. HostingUnit cannot be in All regions");
+                throw new Exception("Enum input illegal. HostingUnit cannot be in All regions");
 
 
 
@@ -106,43 +97,36 @@ namespace BL
 
         public void addOrder(BE.Order order)
         {
-            BE.GuestRequest GR = getGuestRequestByID(order.GuestRequestKey);
-            BE.HostingUnit HU = getHostingUnitByID(order.HostingUnitKey); // בהנחה שזה קיים אחרת לא נשלחה בקשה לפונקציה זו
 
-            if (GR == null)
-            {
-                throw new System.ArgumentNullException(string.Format("worng input {0} not exsits ", order.GuestRequestKey));
+            BE.HostingUnit unit = DAL.getUnitByKey(order.HostingUnitKey);
 
-            }
+            BE.GuestRequest unitK = DAL.getUnitByKey(order.HostingUnitKey);
 
-            if (!(GR.Status==BE.StatusGREnum.פתוחה))
-            {
-                throw new System.Diagnostics.Eventing.Reader.EventLogInvalidDataException(string.Format("This Guest request {0} are close ", order.GuestRequestKey));
-
-            }
-
-            if (GR.Type!=HU.Type)
-            {
-                throw new System.ArgumentNullException(string.Format("the Guest request and the Hosting Unit are not fit in Type parmater  "));
-            }
-            if ((!(GR.Area == BE.AreaEnum.All)) && (!(GR.Area == HU.Area)) )
-            {
-                throw new System.ArgumentNullException(string.Format("the Guest request and the Hosting Unit are not fit in Area parmater  "));
-            }
+            //ליצור השוואת שדות. אם זה לא מתאים לזרוק אקספשיין
 
 
-            //לבדוק לוגית
-            if ((!((HU.Pool) && (GR.Pool == BE.AttractionsEnum.הכרחי)) ||   !(((HU.Pool)) && (GR.Pool == BE.AttractionsEnum.לא_מעוניין))))
-            {
 
-            }
+            // ייצוג הזמנות רלוונטיות בלבד 
 
+            var fit = from GS in GetGuestRequestList()
+                      from HU in GetHostingUnit()
+                      where (GS.Status == BE.StatusGREnum.פתוחה) &&
+                            (GS.Type == HU.Type) &&
+                            ((GS.Area == BE.AreaEnum.All) || (GS.Area == HU.Area)) &&
 
-            // &&
-            //////                            (((HU.Jacuzzi) && (GS.Jacuzzi == BE.AttractionsEnum.הכרחי)) || (GS.Jacuzzi == BE.AttractionsEnum.אפשרי) || ((!(HU.Jacuzzi)) && (GS.Jacuzzi == BE.AttractionsEnum.לא_מעוניין))) &&
-            //////                            (((HU.Garden) && (GS.Garden == BE.AttractionsEnum.הכרחי)) || (GS.Garden == BE.AttractionsEnum.אפשרי) || ((!(HU.Garden)) && (GS.Garden == BE.AttractionsEnum.לא_מעוניין))) &&
-            //////                            (((HU.ChildrensAttractions) && (GS.ChildrensAttractions == BE.AttractionsEnum.הכרחי)) || (GS.ChildrensAttractions == BE.AttractionsEnum.אפשרי) || ((!(HU.ChildrensAttractions)) && (GS.ChildrensAttractions == BE.AttractionsEnum.לא_מעוניין))) &&
+                            /* ביחידת אירוח או שיש אטרקאציה או שאין. אם יש, אז אם בדירשת אירוח היא הכרחית יש התאמה.
+                            אם האטראקציה לא הכרחית אז לא משנה אם יש או אין.
+                            אם האורח לא מעוניין, אז נתאים רק אם אין את הארטאקציה.*/
+                            (((HU.Pool) && (GS.Pool == BE.AttractionsEnum.הכרחי)) || (GS.Pool == BE.AttractionsEnum.אפשרי) || ((!(HU.Pool)) && (GS.Pool == BE.AttractionsEnum.לא_מעוניין))) &&
+                            (((HU.Jacuzzi) && (GS.Jacuzzi == BE.AttractionsEnum.הכרחי)) || (GS.Jacuzzi == BE.AttractionsEnum.אפשרי) || ((!(HU.Jacuzzi)) && (GS.Jacuzzi == BE.AttractionsEnum.לא_מעוניין))) &&
+                            (((HU.Garden) && (GS.Garden == BE.AttractionsEnum.הכרחי)) || (GS.Garden == BE.AttractionsEnum.אפשרי) || ((!(HU.Garden)) && (GS.Garden == BE.AttractionsEnum.לא_מעוניין))) &&
+                            (((HU.ChildrensAttractions) && (GS.ChildrensAttractions == BE.AttractionsEnum.הכרחי)) || (GS.ChildrensAttractions == BE.AttractionsEnum.אפשרי) || ((!(HU.ChildrensAttractions)) && (GS.ChildrensAttractions == BE.AttractionsEnum.לא_מעוניין))) &&
 
+                            ///יש לממש לפי תאריך - אם פנוי 
+                            ///יש לממש 
+                            ///
+                            ///לבדוק לוגיקה
+                      select new Order()/*{GS.GuestRequestKey,HU.HostingUnitKey };*/{ GuestRequestKey = GS.GuestRequestKey, HostingUnitKey = HU.HostingUnitKey, Status = BE.StatusEnum.טרם_טופל, CreateDate = DateTime.Now };
 
 
             throw new NotImplementedException();
@@ -253,22 +237,6 @@ namespace BL
         }
 
 
-        public BE.GuestRequest getGuestRequestByID(int ID)
-        {
-
-            var list = from item in GetGuestRequestList()
-                       where item.GuestRequestKey == ID
-                       select item;
-            foreach (var item in list)
-            {
-                return item.Clone();
-            }
-
-            return null;
-
-        }
-
-
 
 
 
@@ -336,43 +304,3 @@ namespace BL
 
 
 }
-
-
-
-///////*
-////// * 
-////// * 
-////// *            // ייצוג הזמנות רלוונטיות בלבד 
-///
-
-//            //BE.HostingUnit unit = DAL.getUnitByKey(order.HostingUnitKey);
-
-// BE.GuestRequest unitK = DAL.getUnitByKey(order.HostingUnitKey);
-
-//ליצור השוואת שדות. אם זה לא מתאים לזרוק אקספשיין
-
-
-// new Order()/*{GS.GuestRequestKey,HU.HostingUnitKey };*/{ GuestRequestKey = GS.GuestRequestKey, HostingUnitKey = HU.HostingUnitKey, Status = BE.StatusEnum.טרם_טופל, CreateDate = DateTime.Now };
-
-
-//////            var fit = from GS in GetGuestRequestList()
-//////                      from HU in GetHostingUnit()
-//////                      where (GS.Status == BE.StatusGREnum.פתוחה) &&
-//////                            (GS.Type == HU.Type) &&
-//////                            ((GS.Area == BE.AreaEnum.All) || (GS.Area == HU.Area)) &&
-
-//////                            /* ביחידת אירוח או שיש אטרקאציה או שאין. אם יש, אז אם בדירשת אירוח היא הכרחית יש התאמה.
-//////                            אם האטראקציה לא הכרחית אז לא משנה אם יש או אין.
-//////                            אם האורח לא מעוניין, אז נתאים רק אם אין את הארטאקציה.*/
-//////(((HU.Pool) && (GS.Pool == BE.AttractionsEnum.הכרחי)) || (GS.Pool == BE.AttractionsEnum.אפשרי) || ((!(HU.Pool)) && (GS.Pool == BE.AttractionsEnum.לא_מעוניין))) &&
-//////                            (((HU.Jacuzzi) && (GS.Jacuzzi == BE.AttractionsEnum.הכרחי)) || (GS.Jacuzzi == BE.AttractionsEnum.אפשרי) || ((!(HU.Jacuzzi)) && (GS.Jacuzzi == BE.AttractionsEnum.לא_מעוניין))) &&
-//////                            (((HU.Garden) && (GS.Garden == BE.AttractionsEnum.הכרחי)) || (GS.Garden == BE.AttractionsEnum.אפשרי) || ((!(HU.Garden)) && (GS.Garden == BE.AttractionsEnum.לא_מעוניין))) &&
-//////                            (((HU.ChildrensAttractions) && (GS.ChildrensAttractions == BE.AttractionsEnum.הכרחי)) || (GS.ChildrensAttractions == BE.AttractionsEnum.אפשרי) || ((!(HU.ChildrensAttractions)) && (GS.ChildrensAttractions == BE.AttractionsEnum.לא_מעוניין))) &&
-
-//////                            ///יש לממש לפי תאריך - אם פנוי 
-//////                            ///יש לממש 
-//////                            ///
-//////                            ///לבדוק לוגיקה
-//////                      select new Order()/*{GS.GuestRequestKey,HU.HostingUnitKey };*/{ GuestRequestKey = GS.GuestRequestKey, HostingUnitKey = HU.HostingUnitKey, Status = BE.StatusEnum.טרם_טופל, CreateDate = DateTime.Now };
-
-////// */
