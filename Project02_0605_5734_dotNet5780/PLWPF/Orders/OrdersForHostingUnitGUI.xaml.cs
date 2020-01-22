4fac09f6be8dffb3afe52297bf85960d962e671c
Garusi <mgarusi101@gmail.com>
2020-01-22 13:42:21 +0200

    updateupdate

--- Project02_0605_5734_dotNet5780/PLWPF/Orders/OrdersForHostingUnitGUI.xaml.cs
+++ Project02_0605_5734_dotNet5780/PLWPF/Orders/OrdersForHostingUnitGUI.xaml.cs
@@ -14,14 +14,33 @@ using System.Windows.Shapes;
 
 namespace PLWPF.Orders
 {
+  
     /// <summary>
     /// Interaction logic for OrdersForHostingUnitGUI.xaml
     /// </summary>
     public partial class OrdersForHostingUnitGUI : Window
     {
-        public OrdersForHostingUnitGUI()
+        BL.imp_BL bl;
+        BE.HostingUnit HUshow;
+        
+        public OrdersForHostingUnitGUI(BE.HostingUnit HUshow)
         {
             InitializeComponent();
+            IEnumerable <BE.GuestRequest> GRL = bl.GetGuestRequestList();
+            int f = bl.GetGuestRequestList().Count();
+            foreach (var item in GRL)
+            {
+
+            }
+
+            {
+
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
