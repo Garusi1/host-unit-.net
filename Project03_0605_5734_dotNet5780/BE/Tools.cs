d7da171be24dee6b1611574a314dcda3ee3b6176
Salay Shlaom Shuker <shuker@g.jct.ac.il>
2020-01-28 20:15:46 +0200

    סולשיין חדש!

---
+++ Project03_0605_5734_dotNet5780/BE/Tools.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Threading;
+
+
+
+namespace BE
+{
+
+    public static class Tools
+    {
+
+
+
+
+        ///// <summary>
+        ///// deep cloning
+        ///// </summary>
+        ///// <typeparam name="T"></typeparam>
+        ///// <param name="source"></param>
+        ///// <returns></returns>
+        //public static T Clone<T>(this T source)
+        //{
+        //    if (source == null)
+        //        return default(T);
+        //    if (!typeof(T).IsSerializable)
+        //        throw new ArgumentException("The type must be serializable.", "source");
+        //    IFormatter formatter = new BinaryFormatter();  // serialize or convert the object to a binary format
+        //    Stream stream = new MemoryStream(); //כאן אפשר גם לשלח לקובץ עם נשמתמש בזרימה בהפנייה אחרת
+        //    using (stream)
+        //    {
+        //        formatter.Serialize(stream, source); //מעבירים מהמקור לזרימת הנתונים
+        //        stream.Seek(0, SeekOrigin.Begin);
+        //        return (T)formatter.Deserialize(stream);
+        //    }
+        //}
+
+
+        //public static T Clone<T>(this T source)
+        //{
+        //    var isNotSerializable = !typeof(T).IsSerializable;
+        //    if (isNotSerializable)
+        //        throw new ArgumentException("The type must be serializable.", "source");
+        //    var sourceIsNull = ReferenceEquals(source, null);
+        //    if (sourceIsNull)
+        //        return default(T);
+        //    var formatter = new BinaryFormatter();
+        //    using (var stream = new MemoryStream())
+        //    {
+        //        formatter.Serialize(stream, source);
+        //        stream.Seek(0, SeekOrigin.Begin);
+        //        return (T)formatter.Deserialize(stream);
+        //    }
+        //}
+
+
+
+
+        ///// <summary>
+        ///// deep cloning
+        ///// </summary>
+        ///// <typeparam name="T"></typeparam>
+        ///// <param name="source"></param>
+        ///// <returns></returns>
+        //public static T Clone<T>(this T source)
+        //{
+        //    if (source == null)
+        //        return default(T);
+        //    if (!typeof(T).IsSerializable)
+        //        throw new ArgumentException("The type must be serializable.", "source");
+        //    IFormatter formatter = new BinaryFormatter();
+        //    Stream stream = new MemoryStream();
+        //    using (stream)
+        //    {
+        //        formatter.Serialize(stream, source);
+        //        stream.Seek(0, SeekOrigin.Begin);
+        //        return (T)formatter.Deserialize(stream);
+        //    }
+        //}
+
+
+         /// <summary>
+         /// להעביר לשכבת bl 
+         /// </summary>
+         /// <param name="IDNum"></param>
+         /// <returns></returns>
+         
+        public static bool ValidateID(string IDNum)
+        {
+            // Validate correct input
+            if (!System.Text.RegularExpressions.Regex.IsMatch(IDNum, @"^\d{5,9}$"))
+                return false;
+
+            // The number is too short - add leading 0000
+            while (IDNum.Length < 9)
+                IDNum = '0' + IDNum;
+
+            // CHECK THE ID NUMBER
+            int mone = 0;
+            int incNum;
+            for (int i = 0; i < 9; i++)
+            {
+                incNum = Convert.ToInt32(IDNum[i].ToString()) * ((i % 2) + 1);
+                if (incNum > 9)
+                    incNum -= 9;
+                mone += incNum;
+            }
+            return (mone % 10 == 0);
+        }
+
+
+
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Serializable]
+        public class UnLogicException : Exception
+        {
+            public int capacity { get; private set; }
+            public UnLogicException() : base() { }
+            public UnLogicException(string message) : base(message) { }
+            public UnLogicException(string message, Exception inner) : base(message, inner) { }
+            protected UnLogicException(SerializationInfo info, StreamingContext context)
+            : base(info, context) { }
+            // special constructor for our custom exception
+
+            override public string ToString()
+            {
+                return "UnLogicException: שגיאה לוגית  " + Message;
+            }
+        }
+
+
+    }
+}
---
+++ Project03_0605_5734_dotNet5780/PLWPF/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PLWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            /// טעינת שפה עברית לחלונות  ---
+            Uri dictUri = new Uri(@"/res/languages/AppStrings_HE.xaml", UriKind.Relative); 
+            ResourceDictionary resourceDict = Application.LoadComponent(dictUri) as ResourceDictionary;
+            Application.Current.Resources.MergedDictionaries.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+            // ---- עד כאן שפה
+        }
+
+
+        private void openGuestGui(object sender, RoutedEventArgs e) //1.0
+        {
+            GuestGUI GuestGUIShow = new GuestGUI();
+            GuestGUIShow.ShowDialog();//פתיחה באופן זה במחייב התייחסות לחלון זה ולא מאפשר להשתמש בחלון שקרא לו.
+
+        }
+
+        private void openMainHostingUnitGUI(object sender, RoutedEventArgs e)//2.0
+        {
+
+            MainHostingUnitGUI HostingUnitGUIShow = new MainHostingUnitGUI();
+            try
+            {
+
+            HostingUnitGUIShow.ShowDialog();//פתיחה באופן זה במחייב התייחסות לחלון זה ולא מאפשר להשתמש בחלון שקרא לו.
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+
+
+            //add hosing unit gui //2.1
+            //enter to personal area gui - need to recive int id for hosing unit key//2.2
+
+        }
+        private void openMainWebManagerGUI(object sender, RoutedEventArgs e)//4.0
+        {
+
+            MainWebManagerGUI MainWebManagerGUIShow = new MainWebManagerGUI();
+            MainWebManagerGUIShow.Show();
+
+
+            // add 
+            /*
+             * שאילתות רשימת לקוחות 4.1
+7. 4.2 שאילתות יחידת אירוח
+8. 4.3 שאילתות רשימת הזמנות
+4.4 שאילתות נוספות
+             */
+
+        }
+
+    }
+}
