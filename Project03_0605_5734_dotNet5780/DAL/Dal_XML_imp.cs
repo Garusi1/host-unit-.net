d0c14b88758e797c499c8ce791c4f723279b0574
Salay Shlaom Shuker <shuker@g.jct.ac.il>
2020-01-30 12:27:51 +0200

    מקווה שזה לא ידפוק. ואם כן דפק נקח את שלך

--- Project03_0605_5734_dotNet5780/DAL/Dal_XML_imp.cs
+++ Project03_0605_5734_dotNet5780/DAL/Dal_XML_imp.cs
@@ -1,13 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace DAL
 {
-    class Dal_XML_imp
+    public class Dal_XML_imp
     {
+        //public void XmlSample()
+        //{
+        //    if (!File.Exists(FPath))
+        //        CreateFiles();
+        //}
+        //private void CreateFiles()
+        //{
+        //    studentRoot = new XElement("students");
+        //}
+
+
+            /// <summary>
+            /// יצירה זמנית ומהירה של xml ממה שיש כבר בdata
+            /// </summary>
+        public void tempCreate ()
+        {
+            //DataSource ds = new DataSource();
+
+            //XDocument xmlDoc= new XDocument(
+            //    new XDeclaration("1.0","utf-8","yes"),
+            //    new XComment("מתוך מחלקה לאקס אמ אל"),
+            //    new XElement("GuestRequests", //שם שורש
+            //        from p in ds.getGuestRequestList()
+            //        select new XElement("GuestRequestKey", p.GuestRequestKey),
+            //        new XElement (,p),
+
+
+        }
 
     }
 }
